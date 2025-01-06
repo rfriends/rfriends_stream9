0c70afd27e65e85fd4ee07be7da0985508364908
mapi <rfriends2017@ymail.ne.jp>
2025-01-06 15:01:07 +0900

    Update rfriends3_stream9.sh

--- rfriends3_stream9.sh
+++ rfriends3_stream9.sh
@@ -35,7 +35,7 @@ sudo yum -y config-manager --set-enabled crb
 #sudo yum -y install --nogpgcheck https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm -y
 
 # -----------------------------------------
-optlighttpd="off"
+optlighttpd="on"
 optsamba="on"
 export optlighttpd
 export optsamba
